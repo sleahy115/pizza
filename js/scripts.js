edc90e785f4a8a8ab3764525521fe0bb8e0544ed
Sarah Leahy <sleahy115@gmail.com>
2017-01-27 11:17:12 -0800

    alert a price based on the toppings and size of pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,65 @@
-pizzaPrice = 15;
+var pizzaPrice = 15;
 
 function Pizza(size, toppings) {
-    this.size = size;
-    this.toppings = toppings;
+    this.pizzaSize = size;
+    this.toppings = [];
 }
-Pizza.prototype.price = function(size, toppings) {
-    if (pizzaSize === xl) {
-        return pizzaPrice + 2;
-    } else if (pizzaSize === lg) {
-        return pizzaPrice + 1;
-    } else if (pizzaSize === small) {
-        return pizzaPrice - 1;
-    }
-    var toppingsPrice = (numOfToppings * .50);
 
-    return sizePrice + toppingsPrice;
+function sizeCost(size) {
+  if  (size === "xl") {
+      return pizzaPrice + 2;
+    } else if  (size === "large") {
+      return pizzaPrice + 1;
+    } else if (size === "small") {
+      return pizzaPrice - 1;
+    } else {
+      return pizzaPrice;
+    }
 }
+
+function toppingsPrice(toppings) {
+   var toppingsCost = (toppings * .50);
+   return toppingsCost;
+}
+
+function totalPrice(tcost,scost) {
+  var total = toppingsPrice + sizeCost;
+  return total;
+}
+
+// Pizza.prototype.totalPrice = function() {
+//
+// }
+
+// Pizza.prototype.toppingsPrice = function(toppings) {
+// }
+// Pizza.prototype.totalPrice = function(toppingsPrice, sizePrice) {
+// }
 
 $(function() {
-    $("#form.order-form").submit(function(event) {
-        event.preventDefault();
-        var sizeInput = $("input#pizza-size").val();
-        $("input:checkbox[name=pizza-toppings]": checked).each(function() {
-            var toppingsInput = $(this).val();
-        });
+  $("form.order-form").submit(function(event) {
+    event.preventDefault();
+    var sizeInput = $("#pizza-size option:selected").val();
+
+    $("input:checkbox[name=toppings]:checked").each(function() {
+    var toppingsInput = $(this).val();
+    var countToppings = $("[type='checkbox']:checked").length;
+
+    var sCost = sizeCost(sizeInput);
+
+    var tCost = toppingsPrice(countToppings);
+
+    var totalCost = totalPrice(tCost,sCost);
+    alert(totalCost);
+    //
+    // var newPizza = new Pizza(sizeInput, 0);
+    // // alert(newPizza);
+    // newPizza.toppings.push(countToppings);
+    // newPizza.pizzaSize = sizeInput;
+    //
+    //
+    // // $(".output-stream").empty();
+    // $(".output-stream").append("<p>" + sizeInput + " " + toppingsInput + " " + countToppings + totalPrice + "</p>");
     });
+  });
 });
