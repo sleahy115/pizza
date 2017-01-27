ce9798c285d233170cf2e70cb8951e462728d017
sarah leahy <sarah leahy>
2017-01-26 21:50:38 -0700

    create JS/CSS/Img folders and README. Insert basic specifications into readme and create pizza object in JS

---
+++ README.md
@@ -0,0 +1,13 @@
+specs:
+
+select pizza size [small, med, lg, xl]
+
+small pizza = $14
+medium pizza price = $15
+large pizza = $16
+extra large pizza = $17
+
+ select toppings -[meats, veggies, specialty]
+ topping .50 each
+
+ price = size + toppings
---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+pizzaPrice = 15;
+
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
+pizza.prototype.cost (price) {
+  var sizePrice = function () {
+  if (pizzaSize === xl) {
+     pizzaPrice + 2;
+     return pizzaPrice;
+  } else if { (pizzaSize === lg) {
+    pizzaPrice + 1;
+    return pizzaPrice;
+  } else if { (pizzaSize = small)
+    pizzaPrice -1;
+    return pizzaPrice;
+  }
+  var toppingsPrice = (# of toppings * .50)
+
+  sizePrice + toppingsPrice = price;
+}
