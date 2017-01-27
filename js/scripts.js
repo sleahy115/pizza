6c918981b25d9be361866fbf9b20e6bad9f9c079
Sarah Leahy <sleahy115@gmail.com>
2017-01-27 08:19:22 -0800

    create input form for toppings and pizza size

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,28 @@
 pizzaPrice = 15;
 
 function Pizza(size, toppings) {
-  this.size = size;
-  this.toppings = toppings;
+    this.size = size;
+    this.toppings = toppings;
 }
-Pizza.prototype.price = function(size,toppings) {
-  if (pizzaSize === xl) {
-     return pizzaPrice + 2;
-  } else if (pizzaSize === lg) {
-    return pizzaPrice + 1;
-  } else if (pizzaSize === small) {
-    return pizzaPrice - 1;
-  }
-  var toppingsPrice = (numOfToppings * .50);
+Pizza.prototype.price = function(size, toppings) {
+    if (pizzaSize === xl) {
+        return pizzaPrice + 2;
+    } else if (pizzaSize === lg) {
+        return pizzaPrice + 1;
+    } else if (pizzaSize === small) {
+        return pizzaPrice - 1;
+    }
+    var toppingsPrice = (numOfToppings * .50);
 
-  return sizePrice + toppingsPrice;
+    return sizePrice + toppingsPrice;
 }
 
 $(function() {
- $("#form.order").submit(function(){
-  var sizeInput = $("input#size").val();
-  var toppingsInput = $("input#toppings").val();
- });
+    $("#form.order-form").submit(function(event) {
+        event.preventDefault();
+        var sizeInput = $("input#pizza-size").val();
+        $("input:checkbox[name=pizza-toppings]": checked).each(function() {
+            var toppingsInput = $(this).val();
+        });
+    });
 });
