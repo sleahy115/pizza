7dcefe8e0a33fe88893ca3eba4feada5611ca04d
sarah leahy <sarah leahy>
2017-02-01 08:55:37 -0700

    create working prototype for price

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 var pizzaPrice = 15;
+var size="";
+var toppings=[];
 
 function Pizza(size, toppings, cheese) {
     this.pizzaSize = size;
@@ -6,27 +8,31 @@ function Pizza(size, toppings, cheese) {
     this.cheese = cheese;
 }
 
-function sizeCost(size) {
-    if (size === "xl") {
-        return pizzaPrice + 2;
-    } else if (size === "large") {
-        return pizzaPrice + 1;
-    } else if (size === "small") {
-        return pizzaPrice - 1;
-    } else {
-        return pizzaPrice;
-    }
-}
-
-function toppingsPrice(toppings) {
-    var toppingsCost = (toppings * .50);
-    return toppingsCost;
-}
 Pizza.prototype.totalPrice = function() {
-var total = this.toppingsPrice + this.sizeCost;
-parseInt(total,10);
-return total;
+   if (size === "xl") {
+      return pizzaPrice + 2;
+  } else if (size === "large") {
+      return pizzaPrice + 1;
+  } else if (size === "small") {
+      return pizzaPrice - 1;
+  } else {
+      return pizzaPrice;
+  }
+  for (i = 0; i<=toppings.length; i++) {
+    var totalPrice= pizzaPrice + .50;
+  }
+  return pizzaPrice;
 }
+//
+// function sizeCost(size) {
+//
+// }
+//
+// function toppingsPrice(toppings) {
+//     var toppingsCost = (toppings * .50);
+//     return toppingsCost;
+// }
+
 
 $(function() {
     $("#begin-order").click(function() {
@@ -40,15 +46,15 @@ $(function() {
         $("input:checkbox[name=toppings]:checked").each(function() {
             var toppingsInput = $(this).val();
             var countToppings = $("[type='checkbox']:checked").length;
-            var tCost = toppingsPrice(countToppings);
-            var sCost = sizeCost(sizeInput);
-            var totalCost = tCost + sCost;
+            // var tCost = toppingsPrice(countToppings);
+            // var sCost = sizeCost(sizeInput);
+            // var totalCost = tCost + sCost;
             var newPizza = new Pizza(sizeInput, toppingsInput, cheeseInput);
             newPizza.toppings.push(toppingsInput);
             $(".toppings").append(newPizza.toppings + '<br>');
             $(".size").text(newPizza.pizzaSize);
             $(".cheese").text(newPizza.cheese);
-            $(".price").text('$' + totalCost);
+            // $(".price").text('$' + totalCost);
             $(".price-test").text(newPizza.totalPrice);
         });
     });
