1160bee9de2e0af8c78ea800822542adc1af8aba
sarah leahy <sarah leahy>
2017-02-03 11:15:44 -0700

    allow prototype to accept pizza size

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,33 @@
-function Pizza(size, toppings, cheese) {
+function Pizza(size, toppings, cheese,totalPrice) {
     this.pizzaSize = size;
-    console.log(this.size)
-    this.toppings = [];
+    this.toppings = toppings;
     console.log(this.toppings)
     this.cheese = cheese;
+    this.totalPrice= 15;
+    console.log(this.totalPrice)
 }
 
 
-Pizza.prototype.totalPrice = function() {
-  var pizzaPrice = 15;
-   if (this.pizzaSizesize === "xl") {
-      return pizzaPrice + 2;
+Pizza.prototype.pizzaCost = function() {
+  console.log(this.toppings)
+  for (i = 0; i<=this.toppings.length; i++) {
+      this.totalPrice + .50;
+      console.log(this.totalPrice)
+  }
+  return this.totalPrice;
+  console.log(this.totalPrice)
+
+   if (this.pizzaSize === "xl") {
+      return this.totalPrice + 2;
   } else if (this.pizzaSize === "large") {
-      return pizzaPrice + 1;
+      return this.totalPrice + 1;
   } else if (this.pizzaSize === "small") {
-      return pizzaPrice - 1;
+      return this.totalPrice - 1;
   } else {
-      return pizzaPrice;
-      console.log(pizzaPrice)
+      return this.totalPrice;
+      console.log(this.totalPrice)
   }
-  for (i = 0; i<=toppings.length; i++) {
-      pizzaPrice + .50;
-  }
-  return pizzaPrice;
-  console.log(pizzaPrice)
+
 }
 
 $(function() {
@@ -35,19 +39,19 @@ $(function() {
         event.preventDefault();
         var sizeInput = $("#pizza-size option:selected").val();
         var cheeseInput = $("#cheese-choices option:selected").val();
-
+        var totalPrice =0;
         var toppingsInput = [];
-        var toppingsString = "";
          $("input:checkbox[name=toppings]:checked").each(function() {
             toppingsInput.push($(this).val());
-            console.log(toppingsInput)
             });
-        var newPizza = new Pizza(sizeInput, toppingsInput, cheeseInput);
+        var newPizza = new Pizza(sizeInput, toppingsInput, cheeseInput, 15);
             newPizza.toppings = toppingsInput;
-            console.log(newPizza.toppings)
+            console.log(newPizza)
+            totalPrice = newPizza.pizzaCost();
+            console.log(totalPrice)
             $(".toppings").append(newPizza.toppings + '<br>');
             $(".size").text(newPizza.pizzaSize);
             $(".cheese").text(newPizza.cheese);
-            $(".price-test").text(newPizza.totalPrice);
+            $(".price-test").text(totalPrice);
     });
 });
